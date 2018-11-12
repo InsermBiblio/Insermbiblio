d0fe9bde8495148471b1f00e207b37605af723ec
fjoin <florence.join@gmail.com>
2018-11-12 16:03:09 +0100

    gate2 en proxy.insermbiblio

--- inserm/config.php
+++ inserm/config.php
@@ -2,7 +2,7 @@
 /* DFichier configuration générale */
 $config = [
     'ezproxy' => [
-        'inist' => 'http://gate2.inist.fr/login?url=',
+        'inist' => 'http://proxy.insermbiblio.inist.fr/login?url=',
         'ebsco' => 'http://search.ebscohost.com.gate2.inist.fr/login.aspx',
     ]
 ];
