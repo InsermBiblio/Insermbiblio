8dd3aeb941327d8a3a834ea50b6ef2389a36b850
fjoin <florence.join@gmail.com>
2018-12-04 13:45:30 +0100

    fix Warning: Use of undefined constant

--- inserm/database.php
+++ inserm/database.php
@@ -23,11 +23,11 @@ $context['infoFooter'] = Timber::get_posts(['category_name' => 'footer-info']);
 
 $context['basesThematiques'] = [
 		'title' => get_category_by_slug('bases_thematiques')->name,
-		'posts' => Timber::get_posts(['category_name' => bases_thematiques, 'orderby' => 'name', 'order' => 'ASC'])
+		'posts' => Timber::get_posts(['category_name' => 'bases_thematiques', 'orderby' => 'name', 'order' => 'ASC'])
 ];
 $context['ebooksWiley'] = [
 		'title' => get_category_by_slug('e_books_wiley')->name,
-		'posts' => Timber::get_posts(['category_name' => e_books_wiley, 'orderby' => 'name', 'order' => 'ASC'])
+		'posts' => Timber::get_posts(['category_name' => 'e_books_wiley', 'orderby' => 'name', 'order' => 'ASC'])
 ];
 
 Timber::render('database.twig', $context);
